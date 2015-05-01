e0648ba0ae50f9c2c82fdb66c7346d88fc9e0a99
Kevin Chen <kevc.dev@gmail.com>
2015-05-01 15:27:47 -0700

    Fix spacing

--- lib/definition.rb
+++ lib/definition.rb
@@ -2,6 +2,6 @@ class Definition
 	attr_reader(:definition_entry)
 
 	define_method(:initialize) do |attributes|
-			@definition_entry = attributes.fetch(:definition_entry)
+		@definition_entry = attributes.fetch(:definition_entry)
 	end
 end
