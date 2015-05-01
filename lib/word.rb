8fb84e2fe19641a4957d5953353a14e5f8135cdd
Kevin Chen <kevc.dev@gmail.com>
2015-05-01 10:15:13 -0700

    Give Word class save functionality

--- lib/word.rb
+++ lib/word.rb
@@ -1,7 +1,20 @@
 class Word
 	attr_reader(:word_entry)
+	@@word_list = []
 
 	define_method(:initialize) do |attributes|
 		@word_entry = attributes.fetch(:word_entry)
+	end
+
+	define_singleton_method(:clear) do
+		@@word_list = []
+	end
+
+	define_method(:save) do
+		@@word_list.push(self)
+	end
+
+	define_singleton_method(:all) do 
+		@@word_list
 	end
 end
--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -4,10 +4,47 @@ require('definition')
 
 # Words should be able to hold multiple definitions
 describe(Word) do
+
+	before do
+		Word.clear()
+	end
+
 	describe('#initialize') do
-		it('returns the word') do
+		it("returns the word") do
 			my_word = Word.new(:word_entry => 'Toast')
 			expect(my_word.word_entry()).to(eq('Toast'))
 		end
 	end
+
+	describe('#save') do
+		it("saves the word into an array") do 
+			my_word = Word.new(:word_entry => 'Toast')
+			my_word.save()
+			expect(Word.all()).to(eq([my_word]))
+		end
+	end
+
+	describe('.clear') do
+		it("clears the array of words") do 
+			my_word = Word.new(:word_entry => 'Toast')
+			my_word.save()
+			Word.clear()
+			expect(Word.all()).to(eq([]))
+		end
+	end
+
+	describe('.all') do
+		it("should return an empty array at first") do
+			expect(Word.all()).to(eq([]))
+		end
+
+		it("should return and array of words") do
+			my_word = Word.new(:word_entry => 'Toast')
+			my_other_word = Word.new(:word_entry => 'Toast')
+			my_word.save()
+			my_other_word.save()
+			expect(Word.all()).to(eq([my_word, my_other_word]))
+		end
+	end
+
 end
