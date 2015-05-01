8a7fc8a06a97fcd365ab9e454032b6063bd03302
Kevin Chen <kevc.dev@gmail.com>
2015-05-01 16:12:05 -0700

    Write clearer plain english for specs

--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -8,22 +8,33 @@ describe(Word) do
 	before do
 		Word.clear()
 	end
-
+	
+	# Should be a word
 	describe('#initialize') do
 		it("returns the word") do
 			my_word = Word.new({:word_entry => 'Toast'})
 			expect(my_word.word_entry()).to(eq('Toast'))
 		end
 	end
-
+	
+	# Should save the word in a list
 	describe('#save') do
 		it("saves the word into an array") do 
 			my_word = Word.new({:word_entry => 'Toast'})
 			my_word.save()
 			expect(Word.all()).to(eq([my_word]))
 		end
+
+		it("holds multiple words") do
+			my_word = Word.new({:word_entry => 'Toast'})
+			my_word.save()
+			my_other_word = Word.new({:word_entry => 'Jam'})
+			my_other_word.save()
+			expect(Word.all().length()).to(eq(2))
+		end
 	end
 
+	# Should make the list empty
 	describe('.clear') do
 		it("clears the array of words") do 
 			my_word = Word.new({:word_entry => 'Toast'})
@@ -33,6 +44,7 @@ describe(Word) do
 		end
 	end
 
+	# All words should be accessible
 	describe('.all') do
 		it("returns an empty array at first") do
 			expect(Word.all()).to(eq([]))
@@ -47,6 +59,7 @@ describe(Word) do
 		end
 	end
 
+	# Should be able to add multiple definitions to the word
 	describe('#add_definition') do
 		it('returns an empty array at first') do
 			my_word = Word.new({:word_entry => 'Toast'})
@@ -57,19 +70,21 @@ describe(Word) do
 			my_word = Word.new({:word_entry => 'Toast'})
 			my_word_definition = "A grilled slice of bread"
 			my_word.add_definition(my_word_definition)
-			expect(my_word.definition_list().length()).to(eq(1))
+			expect(my_word.definition_list()[0].definition_entry).to(eq(my_word_definition))
 		end
 
-		it('returns all definitions') do
+		it('stores all definitions') do
 			my_word = Word.new({:word_entry => 'Jam'})
 			my_word_definition = "A condiment often put on toast"
 			my_other_word_definition = "A song that one likes very much"
 			my_word.add_definition(my_word_definition)
 			my_word.add_definition(my_other_word_definition)
-			expect(my_word.definition_list().length()).to(eq(2))
+			expect(my_word.definition_list()[0].definition_entry).to(eq(my_word_definition))
+			expect(my_word.definition_list()[1].definition_entry).to(eq(my_other_word_definition))
 		end
 	end
 
+	# Should create identifier for the word in the list
 	describe('#id') do
 		it('returns the correct id for each word') do 
 			my_word = Word.new({:word_entry => 'Toast'})
@@ -80,6 +95,7 @@ describe(Word) do
 		end
 	end
 
+	# Should be able to find the word with the same id 
 	describe('.find') do
 		it('returns the object with the same id') do
 			my_word = Word.new({:word_entry => 'Toast'})
