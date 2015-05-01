92b9ae2dd425cd4648530f88432fc998f09e28bb
Kevin Chen <kevc.dev@gmail.com>
2015-05-01 11:13:26 -0700

    Fix specs for syntax conventions

--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -11,14 +11,14 @@ describe(Word) do
 
 	describe('#initialize') do
 		it("returns the word") do
-			my_word = Word.new(:word_entry => 'Toast')
+			my_word = Word.new({:word_entry => 'Toast'})
 			expect(my_word.word_entry()).to(eq('Toast'))
 		end
 	end
 
 	describe('#save') do
 		it("saves the word into an array") do 
-			my_word = Word.new(:word_entry => 'Toast')
+			my_word = Word.new({:word_entry => 'Toast'})
 			my_word.save()
 			expect(Word.all()).to(eq([my_word]))
 		end
@@ -26,7 +26,7 @@ describe(Word) do
 
 	describe('.clear') do
 		it("clears the array of words") do 
-			my_word = Word.new(:word_entry => 'Toast')
+			my_word = Word.new({:word_entry => 'Toast'})
 			my_word.save()
 			Word.clear()
 			expect(Word.all()).to(eq([]))
@@ -39,9 +39,9 @@ describe(Word) do
 		end
 
 		it("returns and array of words") do
-			my_word = Word.new(:word_entry => 'Toast')
+			my_word = Word.new({:word_entry => 'Toast'})
 			my_word.save()
-			my_other_word = Word.new(:word_entry => 'Jam')
+			my_other_word = Word.new({:word_entry => 'Jam'})
 			my_other_word.save()
 			expect(Word.all()).to(eq([my_word, my_other_word]))
 		end
@@ -49,21 +49,21 @@ describe(Word) do
 
 	describe('#add_definition') do
 		it('returns an empty array at first') do
-			my_word = Word.new(:word_entry => 'Toast')
+			my_word = Word.new({:word_entry => 'Toast'})
 			expect(my_word.definition_list()).to(eq([]))
 		end
 
 		it('returns a definitions') do
-			my_word = Word.new(:word_entry => 'Toast')
-			my_word_definition = Definition.new(:definition_entry => "A grilled slice of bread")
+			my_word = Word.new({:word_entry => 'Toast'})
+			my_word_definition = Definition.new({:definition_entry => "A grilled slice of bread"})
 			my_word.add_definition(my_word_definition)
 			expect(my_word.definition_list()).to(eq([my_word_definition]))
 		end
 
 		it('returns all definitions') do
-			my_word = Word.new(:word_entry => 'Jam')
-			my_word_definition = Definition.new(:definition_entry => "A condiment often put on toast")
-			my_other_word_definition = Definition.new(:definition_entry => "A song that one likes very much")
+			my_word = Word.new({:word_entry => 'Jam'})
+			my_word_definition = Definition.new({:definition_entry => "A condiment often put on toast"})
+			my_other_word_definition = Definition.new({:definition_entry => "A song that one likes very much"})
 			my_word.add_definition(my_word_definition)
 			my_word.add_definition(my_other_word_definition)
 			expect(my_word.definition_list()).to(eq([my_word_definition, my_other_word_definition]))
@@ -72,9 +72,9 @@ describe(Word) do
 
 	describe('#id') do
 		it('returns the correct id for each word') do 
-			my_word = Word.new(:word_entry => 'Toast')
+			my_word = Word.new({:word_entry => 'Toast'})
 			my_word.save()
-			my_other_word = Word.new(:word_entry => 'Jam')
+			my_other_word = Word.new({:word_entry => 'Jam'})
 			my_other_word.save()
 			expect(my_other_word.id()).to(eq(2))
 		end
@@ -82,9 +82,9 @@ describe(Word) do
 
 	describe('.find') do
 		it('returns the object with the same id') do
-			my_word = Word.new(:word_entry => 'Toast')
+			my_word = Word.new({:word_entry => 'Toast'})
 			my_word.save()
-			my_other_word = Word.new(:word_entry => 'Jam')
+			my_other_word = Word.new({:word_entry => 'Jam'})
 			my_other_word.save()
 			expect(Word.find(2)).to(eq(my_other_word))
 		end
